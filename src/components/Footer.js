87d34be7c830e780e6c8051c1de1b8aec1c28b1c
gustavocontin1 <contin.gustavo1@gmail.com>
2022-06-02 15:58:49 -0300

    creacion de navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+return (
+    <footer>
+        <p>Copyright</p>
+    </footer>
+)
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Header = () =>{
+    return (
+        <header>
+            <h1>Depor T</h1>
+            <nav>
+                <a href='#'>Incio</a>
+                <a href='#'>Productos</a>
+                <a href='#'>Contacto</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
